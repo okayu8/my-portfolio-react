046922957daa77591b9f9d2d60674b1d91acf9ee
片岡祐太郎 <hitugaya12@gmail.com>
2019-07-20 20:08:12 +0900

    add UseEffectTest2

--- src/components/Test2.js
+++ src/components/Test2.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import UseEffectTest2 from './test2/UseEffectTest2';
 
 export default function Test1() {
     return (
         <Fragment>
             <h1>Test2</h1>
+            <UseEffectTest2 />
         </Fragment>
     )
 }
